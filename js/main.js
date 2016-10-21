86e139c6f60b3473ea817e3326808b7682d6f4a4
aheimel <amyheimel@Amy-Heimel.local>
2016-10-21 16:52:29 +0200

    add button toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /* 1. Uitschuiven van zoekveld d.m.v. javascript */
 var zoekveld = document.querySelector('.zoekveld');
 var button = document.querySelector('button');
@@ -10,3 +9,17 @@ var active = function () {
 }
 
 button.addEventListener('click', active);
+
+
+/* 1. Meer verhalen weergeven */
+/*
+var meerartikelen = document.querySelector('.artikelen-inactief');
+var knop = document.querySelector('.meerverhalen-knop');
+
+
+var verhalen = function () {
+    meerartikelen.classList.toggle('.artikelen-actief')
+}
+
+knop.addEventListener('click', verhalen);
+
